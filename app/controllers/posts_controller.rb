bd37b8418644011bf6473fe3fa3de7ce99560268
ideadapt <kunz@ideadapt.net>
2013-08-27 23:56:37 +0200

    some more

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
 	def create
 	  @post = Post.new(post_params)
 	  @post.save
-	  reirect to @post
-	end
-	
+	  redirect_to @post
+  end
+
 	private
 	def post_params
           params.require(:post).permit(:title, :longitude, :latitude, :street, :city, :zip, :offer_msg)	
